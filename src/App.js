6cec2d712519e2cd55819a98d11219d741a0acef
inpakuz <saranheochika82@gmail.com>
2024-10-21 13:04:29 +0600

    main route

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
        <Route path='/home' element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
-        <Route path='/main' element={<Main/>}></Route>
+        <Route path='/' element={<Main/>}></Route>
        </Routes>
       </BrowserRouter>
     </div>
